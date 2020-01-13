a2da52e7d1707f5c6dcc171557aa8dfc17ecead7
Penn <matousec@gmail.com>
2020-01-13 14:27:44 +0800

    测试类中去掉启动类配置

--- src/test/java/code/dp/drools/DroolsApplicationTests.java
+++ src/test/java/code/dp/drools/DroolsApplicationTests.java
@@ -3,9 +3,11 @@ package code.dp.drools;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = DroolsApplication.class)
+@SpringBootTest
 public class DroolsApplicationTests {
+
     @Test
     void contextLoads() {
     }
+
 }
